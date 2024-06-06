0d52c794b341985d6bbd89d761533b282647b1fb
keraenarium <kirillreshetnyak@gmail.com>
2024-06-06 10:50:12 +0300

    Add project

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin from "./page/Admin"
+import Header from './components/Header/Header';
+import LogIn from './components/LogIn/LogIn';
+import Registration from './components/Registration/Registration';
+import Basket from './components/Basket/Basket';
+import Chose from './components/Chose/Chose';
+import MainPage from './page/mainPage/mainPage';
+import Footer from './components/Footer/Footer';
+import Product from './components/Product/Product';
+import AllProductForAnimal from './page/AllProductForAnimal';
+import AllProductForDecor from './page/AllProductForDecor';
+import AllProductForMen from './page/AllProductForMen';
+import AllProductForGifs from './page/AllProductForGifs';
+import AllProductForWoman from './page/AllProductForWoman';
+import AllProductForCosmetic from './page/AllProductForCosmetic';
+import MainOrder from "./components/MainOrder/MainOrder"
+import ProductElemPage from './components/ProductElemPage/ProductElemPage';
+import { BasketProvider } from './components/BasketContent/BasketContent';
+import AccountPage from './page/AccountPage';
+import './App.css';
+
+function App() {
+  return (
+    <BasketProvider>
+      <Router>
+        <Header />
+        <div className="App">
+          <Routes>AccountPage
+
+            <Route path="/" element={<MainPage />} />
+            <Route path="/account/:id" element={<AccountPage />} />
+            <Route path="/product/women" element={< AllProductForWoman/>} />
+            <Route path="/product/women/:id" element={<ProductElemPage />} />
+            <Route path="/product/:id" element={<ProductElemPage />} />
+            <Route path="/favorite" element={<Chose />} />
+            <Route path="/basket" element={<Basket />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/order" element={<MainOrder />} />
+            <Route path="/admin" element={<Admin />} />
+            
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </BasketProvider>
+  );
+}
+
+export default App;
---
+++ src/page/Admin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AdminEnter from '../components/AdminEnter/AdminEnter'
+function Admin() {
+  return (
+    <AdminEnter></AdminEnter>
+  )
+}
+
+export default Admin
